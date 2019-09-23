3266c0631f767696b0d7f285bfdaab25133a81fe
ArthurWZH <32083090+ArthurWZH@users.noreply.github.com>
2019-09-23 10:28:58 +0800

    Initial Commit

---
+++ AWDroper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AWDroper
+//
+//  Created by Zihao Arthur Wang [STUDENT] on 9/23/19.
+//  Copyright © 2019 Zihao Arthur Wang [STUDENT]. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AWDroper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AWDroper
+//
+//  Created by Zihao Arthur Wang [STUDENT] on 9/23/19.
+//  Copyright © 2019 Zihao Arthur Wang [STUDENT]. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
